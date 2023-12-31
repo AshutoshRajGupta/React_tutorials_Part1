2735220896115121fc25e10ad6456cc71536bc01
Ashutosh Raj Gupta <ag2364443@gmail.com>
2023-12-31 20:38:42 +0530

    lesson no 4 style css

--- tut/src/Header.js
+++ tut/src/Header.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
-  return (
-    <div>Groceories List</div>
-  )
-}
+//   const headerStyle = {
+//     backgroundColor: "royalblue",
+//     color: "#fff",
+//     fontSize: 30,
+//   };
+  return <header>Groceories List</header>;
+};
 
-export default Header
+export default Header;
+
+// for inline css styles use {{}}
+// second way is to create a folder and called it
