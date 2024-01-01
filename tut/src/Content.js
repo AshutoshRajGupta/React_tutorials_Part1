f4351fed718b3d16a082f9d97aaa234a0f4df7a9
Ashutosh Raj Gupta <ag2364443@gmail.com>
2024-01-01 12:39:26 +0530

    updated lesson no 5 on click events

--- tut/src/Content.js
+++ tut/src/Content.js
@@ -6,8 +6,22 @@ const Content = () => {
         const int =Math.floor(Math.random()*3);
         return names[int];
       }
+
+      const handleCLick=()=>{
+        console.log('you clicked it')
+      }
+      const handleCLick2=(name)=>{
+        console.log(`${name} was clicked`)
+      }
+      const handleCLick3=(e)=>{
+        console.log(e)
+      }
+
   return <div>
-    <p> Hello {handleNameChange()}!!</p>
+    <p onDoubleClick={handleCLick}> Hello {handleNameChange()}!!</p>
+    <button onClick={handleCLick}>Click it</button>
+    <button onClick={()=>handleCLick2("Ashutosh")}>Click it</button>
+    <button onClick={(e)=>handleCLick3(e)}>Click it</button>
   </div>;
 };
 
