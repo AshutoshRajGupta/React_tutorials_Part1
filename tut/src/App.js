baa3edfa48133a5470b1108a80ac78d366a87868
Ashutosh Raj Gupta <ag2364443@gmail.com>
2024-01-04 11:26:07 +0530

    useEffect tutorial changes

--- tut/src/App.js
+++ tut/src/App.js
@@ -2,38 +2,42 @@ import Header from "./Header";
 import Content from "./Content";
 import AddItem from "./AddItem";
 import SearchItem from "./SearchItem";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import Footer from "./Footer";
 
 function App() {
   const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("shoppinglist"))
+    JSON.parse(localStorage.getItem("shoppinglist")) || []
   );
   const [newItem, setNewItem] = useState("");
   const [search, setSearch] = useState("");
 
-  const setAndSaveItems = (newItems) => {
-    setItems(newItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(newItems));
-  };
+  // loading everything from the beginning instead of that we are checking state of the 
+  // item and setting it in localstorage.
+
+  useEffect(()=>{
+    localStorage.setItem('shoppinglist',JSON.stringify(items));
+  },[items])
+
+
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleSubmit = (e) => {
